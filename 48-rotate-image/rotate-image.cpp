6b7731d88a12d9836343a4bb238aa55b20962612
satyam530 <sj5343795@gmail.com>
2026-07-25 10:49:59 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (9.57%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -3,13 +3,14 @@ public:
     void rotate(vector<vector<int>>& arr) {
 
         int n=arr.size();
-              vector<vector<int>> ans(n, vector<int>(n));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                ans[j][n-1-i]=arr[i][j];
-
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                swap(arr[i][j],arr[j][i]);
             }
         }
-        arr=ans;
+      for(int i=0;i<n;i++){
+        reverse(arr[i].begin(),arr[i].end());
+      }
+        
     }
 };
